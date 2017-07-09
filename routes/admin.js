0fd4254fd8363e6c44a87ee087bd16f066db1ea5
John Obi <johnny9334@ymail.com>
2017-07-10 00:08:25 +0100

    fixing admin hash login

--- routes/admin.js
+++ routes/admin.js
@@ -8,6 +8,8 @@ var route = express.Router();
 
 
 
+
+
 //REQUESTING FOR LOGIN PAGE
 route.get('/login', function(req, res) {
     res.render('admin_pages/admin_login.jade');
@@ -23,7 +25,7 @@ route.post('/login', function(req, res) {
             req.flash('error', 'Incorrect username or password');
             res.render('admin_pages/admin_login.jade');
         }else {
-            if (bcrypt.compareSync(req.body.password,admin.password)) {
+            if (req.body.password === admin.password) {
                 req.session.admin = admin.email;
                 req.flash('success', 'Welcome, Admin '+admin.firstname);
                 res.redirect('/admin/admin_dashboard');
